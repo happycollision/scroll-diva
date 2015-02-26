edeb2568f1dd9c95c984c2b43757f88034f7c7a7
Don Denton <don@happycollision.com>
2015-02-26 02:56:01 -0600

    Adds grunt bump

--- Gruntfile.js
+++ Gruntfile.js
@@ -81,6 +81,24 @@ module.exports = function (grunt) {
         tasks: ['jshint:test', /*'qunit'*/]
       }
     },
+    bump: {
+      options: {
+        files: ['package.json'],
+        updateConfigs: ['pkg'],
+        commit: true,
+        commitMessage: 'Bump to v%VERSION%',
+        commitFiles: ['-a'],
+        createTag: true,
+        tagName: 'v%VERSION%',
+        tagMessage: 'Version %VERSION%',
+        push: false,
+        pushTo: 'origin',
+        //gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+        //globalReplace: false,
+        //prereleaseName: false,
+        //regExp: false
+      }
+    },
     connect: {
       server: {
         options: {
