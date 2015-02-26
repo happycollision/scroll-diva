589d3220392ca5840d98e8e177dc1e3b1b51ae10
Don Denton <don@happycollision.com>
2015-02-26 03:14:49 -0600

    Fixing another yeoman bug

--- test/scrolldiva_test.js
+++ test/scrolldiva_test.js
@@ -1,36 +1,62 @@
-(function ($) {
-  module('jQuery#', {
-    setup: function () {
+(function($) {
+  /*
+    ======== A Handy Little QUnit Reference ========
+    http://api.qunitjs.com/
+
+    Test methods:
+      module(name, {[setup][ ,teardown]})
+      test(name, callback)
+      expect(numberOfAssertions)
+      stop(increment)
+      start(decrement)
+    Test assertions:
+      ok(value, [message])
+      equal(actual, expected, [message])
+      notEqual(actual, expected, [message])
+      deepEqual(actual, expected, [message])
+      notDeepEqual(actual, expected, [message])
+      strictEqual(actual, expected, [message])
+      notStrictEqual(actual, expected, [message])
+      throws(block, [expected], [message])
+  */
+
+  module('jQuery#awesome', {
+    // This will run before each test in this module.
+    setup: function() {
       this.elems = $('#qunit-fixture').children();
     }
   });
 
-  test('is chainable', function () {
+  test('is chainable', function() {
     expect(1);
-    strictEqual(this.elems.(), this.elems, 'should be chainable');
+    // Not a bad test to run on collection methods.
+    strictEqual(this.elems.awesome(), this.elems, 'should be chainable');
   });
 
-  test('is ', function () {
+  test('is awesome', function() {
     expect(1);
-    strictEqual(this.elems.().text(), '012', 'should be ');
+    strictEqual(this.elems.awesome().text(), 'awesome0awesome1awesome2', 'should be awesome');
   });
 
-  module('jQuery.');
+  module('jQuery.awesome');
 
-  test('is ', function () {
+  test('is awesome', function() {
     expect(2);
-    strictEqual($.(), '.', 'should be ');
-    strictEqual($.({punctuation: '!'}), '!', 'should be thoroughly ');
+    strictEqual($.awesome(), 'awesome.', 'should be awesome');
+    strictEqual($.awesome({punctuation: '!'}), 'awesome!', 'should be thoroughly awesome');
   });
 
-  module(': selector', {
-    setup: function () {
+  module(':awesome selector', {
+    // This will run before each test in this module.
+    setup: function() {
       this.elems = $('#qunit-fixture').children();
     }
   });
 
-  test('is ', function () {
+  test('is awesome', function() {
     expect(1);
-    deepEqual(this.elems.filter(':').get(), this.elems.last().get());
+    // Use deepEqual & .get() when comparing jQuery objects.
+    deepEqual(this.elems.filter(':awesome').get(), this.elems.last().get(), 'knows awesome when it sees it');
   });
+
 }(jQuery));
