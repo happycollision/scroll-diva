ccc132785d4c99775066e783aee357976aeae263
Don Denton <don@happycollision.com>
2015-02-26 22:43:01 -0600

    Initial scroll diva code

--- src/scrolldiva.js
+++ src/scrolldiva.js
@@ -8,10 +8,89 @@
 (function ($) {
   // Collection method.
   $.fn.scrollDiva = function () {
+    
     return this.each(function (i) {
-      // Do something to each selected element.
-      $(this).html('scrollDiva' + i);
+      function snapToBottom() {
+	      // Pushes the element to to bottom of parent
+	      var newTopMargin = parentHeight - shavedHeight - originalPos.top;
+	      $el.css({
+		      position: 'relative', 
+		      marginTop: newTopMargin,
+		      bottom: 'auto',
+		      left: 'auto',
+		      top: 'auto'
+		     });
+      }
+      function pinToBottom(){
+	      // pins element to bottom of viewport
+	      
+	      $el.css({
+		      position: 'fixed', 
+		      bottom: 0, 
+		      left: originalPos.left,
+		      top: 'auto',
+		      marginTop: originalTopMargin
+		     });
+      }
+      function pinToTop(){
+	      // pins element to top of viewport
+	      console.log('hello pin');
+	      
+	      $el.css({
+		      position: 'fixed', 
+		      bottom: 'auto', 
+		      left: originalPos.left,
+		      top: 0,
+		      marginTop: originalTopMargin
+		     });
+      }
+      function snapToTop(){
+	      // AKA set to original position
+	      console.log('hello snap');
+	      $el.css({
+		      position: 'relative', 
+		      marginTop: originalTopMargin,
+		      bottom: 'auto',
+		      left: 'auto',
+		      top: 'auto'
+		     });
+      }
+
+	    // Set the data for custom selector
+	    $(this).data('scrollDiva',true);
+	    
+	    var $el = $(this);
+      var $window = $(window);
+      var $parent = $el.parent();
+      
+      if ($parent.css('position') === 'static'){
+	      // Set the parent position to relative
+	      // This way we can get the child element's
+	      // position offset from the parent, not from
+	      // the whole document
+	      $parent.css('position', 'relative');
+      }
+
+      var fullHeight = $el.outerHeight(true);
+      var borderWidth = $el.css('borderTopWidth');
+      var originalTopMargin = parseInt($el.css('marginTop'));
+      var originalLeftMargin = parseInt($el.css('marginLeft'));
+      var originalPos = $el.position();
+      var originalOffset = $el.offset();
+      var shavedHeight = fullHeight - originalTopMargin;
+      var parentTopPos = $parent.offset().top;
+      var parentHeight = $parent.height();
+      var parentBottomPos = parentTopPos + parentHeight;
+      
+      $window.scroll(function(){
+	      var scrollPos = parseInt($window.scrollTop() - originalOffset.top + originalTopMargin + borderWidth);
+	      if (scrollPos > 0) { pinToTop(); }
+	      if (scrollPos <= 0) { snapToTop(); }
+	      
+      });
+      
     });
+
   };
 
   // Static method.
